fb2901528b996d82a79dbf2151b657132a8fa858
Maurice <maurice.bigler@gibmit.ch>
2020-06-22 17:44:45 +0200

    Login View

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../views/MainView.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
@@ -20,4 +20,5 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+    launch(args);
 }
